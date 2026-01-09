ed01d973f8068aad71a457bf74649280eb5d3458
WebApp07 <aminezouguig@proton.me>
2026-01-09 01:42:40 +0100

    Setup NeonDB with drizzle

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="text-4xl">
       <h1>Hello World</h1>
-      <Button>Click me</Button>
+      <Button >Click me</Button>
     </main>
   );
 }
